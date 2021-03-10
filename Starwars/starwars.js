1038466c1c7d11f395ca6efcb51b9159297637ea
Josh Yates <joshygt500@gmail.com>
2021-03-09 21:09:17 -0700

    trying to get import films to work

--- Starwars/starwars.js
+++ Starwars/starwars.js
@@ -1,7 +1,5 @@
-import { films } from '../data/films.js'
+import {films} from '../data/films.js'
 
 let itemOne = document.querySelector('#item1');
 
-itemOne.textContent = 'films[2].title'
-
-console.log{films[2].title}
+itemOne.textContent = 'films[0].title'
