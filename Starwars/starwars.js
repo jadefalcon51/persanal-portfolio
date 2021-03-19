a6d685f999d449432088a2b4b4f0dd46b7ec832a
Josh Yates <joshygt500@gmail.com>
2021-03-19 17:20:50 -0600

    get last numer correct films number

--- Starwars/starwars.js
+++ Starwars/starwars.js
@@ -2,9 +2,15 @@ import { films } from '../data/films.js'
 
 let titlelist = document.querySelector('.titleList');
 
-for (var i = 0; i < films.length; i++) {
+for (let i = 0; i < films.length; i++) {
+    const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1))
     let title = films [i].title
     let newItem = document.createElement ('li')
-    newItem.textContent = title
-    titlelist.appendChild(newItem)
+    newItem.textContent = foundFilm.title
+    titlelist.appendChild(newItem)    
+}
+
+function getLastNumber(url) {
+    let end = url[url.length -2]
+    return parseInt(end, 10)
 }
