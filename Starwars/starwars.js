2922adb2e038fbbcd0db2c4b8fbed5b54a1a7a30
Josh Yates <joshygt500@gmail.com>
2021-03-19 18:20:17 -0600

    I understand more

--- Starwars/starwars.js
+++ Starwars/starwars.js
@@ -4,7 +4,7 @@ let titlelist = document.querySelector('.titleList');
 
 for (let i = 0; i < films.length; i++) {
     const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1))
-    let title = films [i].title
+    // let title = foundFilm.title
     let newItem = document.createElement ('li')
     newItem.textContent = foundFilm.title
     titlelist.appendChild(newItem)    
