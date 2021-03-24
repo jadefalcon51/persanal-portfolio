5fe245ae49c86a8db730a7af0b7c44346667ac0f
Josh Yates <joshygt500@gmail.com>
2021-03-24 16:14:22 -0600

    add jaba

--- characters/characters.js
+++ characters/characters.js
@@ -16,10 +16,15 @@ femaleButton.textContent = 'Female Characters'
 femaleButton.addEventListener('click', () => populatDom (femaleCharacters))
 mainHeader.appendChild(femaleButton)
 
+const otherButton = document.createElement('button')
+otherButton.textContent = 'Other Characters'
+otherButton.addEventListener('click', () => populatDom (otherCharacters))
+mainHeader.appendChild(otherButton)
+
 const maleCharacters = people.filter (person => person.gender === 'male')
 const femaleCharacters = people.filter (person => person.gender === 'female')
 const otherCharacters = people.filter (person => {
-    if (person.gender === 'n/a' || person.gender === 'none') {
+    if (person.gender === 'n/a' || person.gender === 'none' || person.gender === 'hermaphrodite') {
         return person
     }
 })
