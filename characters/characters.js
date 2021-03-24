d98c7a366089ad4b3ce4d3ee8a9c2e8e73a826d8
Josh Yates <joshygt500@gmail.com>
2021-03-23 20:43:19 -0600

    start and base format of charactures

---
+++ characters/characters.js
@@ -0,0 +1,26 @@
+import { people } from '../data/people.js'
+
+const mainContent = document.querySelector ('main')
+
+people.forEach (person => {
+    const charFigure = document.createElement('figure')
+    const charImg = document.createElement('img')
+    const charCaption = document.createElement('figcaption')
+    let charNum = getLastNumber(person.url)
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+
+    charCaption.textContent = person.name
+
+    charFigure.appendChild(charImg)
+    charFigure.appendChild(charCaption)
+    mainContent.appendChild(charFigure)
+})
+    
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if (url.charAt(start) === '/') {
+        start ++
+    }
+    return url.slice(start, end)
+}
