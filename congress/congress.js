5b0110419c8681e7cdec4508fbd5acc998c92712
Josh Yates <joshygt500@gmail.com>
2021-04-09 22:14:19 -0600

    imports and const congress and finish html

--- congress/congress.js
+++ congress/congress.js
@@ -1,2 +1,7 @@
 import { senators } from "../data/senators.js"
 import { representatives } from "../data/representatives.js"
+import { removeChildren } from "../Utils/index.js"
+
+const congressGrid = document.querySelector('.congressGrid')
+const seniorityButton = document.querySelector('#seniorityButton')
+const birthdayButton = document.querySelector('#birthdayButton')
