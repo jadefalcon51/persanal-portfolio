8efec6f783dc65988463eed95a974eb3055f3442
Josh Yates <joshygt500@gmail.com>
2021-04-15 12:46:08 -0600

    pokemon start js

---
+++ Pokemon/pokemon.js
@@ -0,0 +1,26 @@
+const pokeGrid = document.querySelector('.pokeGrid')
+const loadButton = document.querySelector('.loadPokemon')
+const fetchButton = document.querySelector('.fetchPokemonByID')
+const newButton = document.querySelector('.newPokemon') 
+
+loadButton.addEventListener('click', () => {
+    loadPage()
+})
+
+fetchButton.addEventListener('click', () => {
+    let pokeId = prompt("pokemon ID of Name:").toLowerCase()
+    getAPIData(`https://pokeapi.co/api/v2/pokemon/${pokeId}`).then(
+        data => populatePokeCard(data)
+    ).catch(error => console.log(error))
+})
+
+class pokemon {
+    constructor(name, height, weight, abilities, moves) {
+        this.id = 900
+        this.name = name
+        this.height = height
+        this.weight = weight
+        this.abilities = abilities
+        this.moves = moves
+    }
+}
